f6e864521ba2226acae226ea85e81f0dd3e0fa16
xiaopow <xiaoyang88@gmail.com>
2021-02-03 13:03:07 +0800

    xiao

--- application.js
+++ application.js
@@ -1,8 +1,9 @@
 var updateSubtotal = function(ele){
-  var itemPrice = parseFloat($(ele).find('.item-price').text());
+  var itemPrice = parseFloat($(ele).find('.item-price').text().trim().slice(1));
   var itemQty = parseFloat($(ele).find('.quantity').val());
 
   var subtotal = itemPrice * itemQty;
+  console.log(itemPrice, itemQty)
   $(ele).children('.item-subtotal').html(subtotal);
 
   // if subtotal is NaN, return 0
@@ -36,21 +37,21 @@ $(document).ready(function(){
   $('#additem').on('click', function(event){
     event.preventDefault();
     var name = $(this).parent().parent().find('[name=name]').val();
-    var cost = $(this).parent().parent().find('[name=cost]').val();
+    var cost = Number($(this).parent().parent().find('[name=cost]').val());
     var c = cost.toFixed(2);
+    
+    if (name.trim() === "" || cost === 0) {
+        alert('Please enter food name and quantity.');
+        return false;
+    }
 
     $('#item-list').append('<div class="row item">' +
     '<div class="item-name col-3 mb-4">' + name + '</div>' +
     '<div class="item-price col-3">' + '$' + c + '</div>' +
     '<div class="item-qty col-3">' + '<label>QTY</label>' +
     '<input class="quantity" type="number">' + '</div>' +
-    '<div class="col-xs-1">' + '<button class="remove">' + 'Remove' +
-    '</button>');
+    '<div class="col-xs-1">' + '<button class="remove">Remove</button></div>' +'<div class="item-subtotal col-2 ml-3">$--.--</div>');
 
-    if ($('input').val() === "") {
-        alert('Please enter food name and quantity.');
-        return false;
-    }
 
     updateTotal();
     $(this).parent().parent().find('[name=name]').val('');
@@ -58,9 +59,10 @@ $(document).ready(function(){
   });
 
     var timeout;
-    $(document).on('input', '.item', function(){
+    $(document).on('input', '.quantity', function(){
       clearTimeout(timeout);
       timeout = setTimeout(function(){
+        console.log('asd')
         updateTotal();
       }, 500)
     })
